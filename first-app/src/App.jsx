8f5cfbb355f5938f68042c24595a2f73d3f70467
MamounB <mamoun2206@gmail.com>
2026-02-23 22:34:07 -0500

    Start Test

---
+++ first-app/src/App.jsx
@@ -0,0 +1,12 @@
+import {StrictMode, useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+    return (
+        <h1>Hey</h1>
+    )
+}
+
+export default App
