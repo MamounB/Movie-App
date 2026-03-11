f2f3e736301407034e5ac718610bf9e02d0e2fad
MamounB <mamoun2206@gmail.com>
2026-03-11 07:48:51 -0400

    Search
    API connection
    
    Database appwrite setup
    
    Search Count Updated

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,11 +1,106 @@
-import {StrictMode, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {StrictMode, useState, useEffect} from 'react'
+import Search from "./Components/Search.jsx";
+import Spinner from "./Components/Spinner.jsx";
+import MovieCard from "./Components/MovieCard.jsx";
+import {updateSearchCount} from "./appwrite.js";
+
+const API_BASE_URL = "https://api.themoviedb.org/3/";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+    method: "GET",
+    headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+    }
+}
 
 const App = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [movieList, setMovieList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedSearchTerm(searchTerm);
+        }, 500);
+
+        return () => clearTimeout(handler);
+    }, [searchTerm]);
+
+    const fetchMovies = async (query = '') => {
+
+        setIsLoading(true);
+        setErrorMessage('');
+
+        try {
+            const endpoint = query
+                ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+            const response = await fetch(endpoint, API_OPTIONS);
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch movies');
+            }
+
+            const data = await response.json();
+
+            if (data.Response === 'False') {
+                setErrorMessage(data.Error || 'Failed to fetch movies');
+                setMovieList([]);
+            }
+
+            setMovieList(data.results || []);
+
+            if (query && data.results.length > 0) {
+                await updateSearchCount(query, data.results[0]);
+            }
+
+        } catch (error) {
+            console.error("Error fetching movies", {error});
+            setErrorMessage('Error fetching movies. Please try again later.');
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies(debouncedSearchTerm);
+    }, [debouncedSearchTerm]);
+
     return (
-        <h1>Hey</h1>
+        <main>
+            <div className="pattern" />
+
+            <div className="wrapper">
+                <img src="/hero.png" alt="Hero Banner" />
+                <header>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                </header>
+
+                <section className="all-movies">
+                    <h2 className="mt-20px" >All movies</h2>
+
+                    {isLoading ? (
+                        <Spinner />
+                    ) : errorMessage ? (
+                        <p className="text-red-500">{errorMessage}</p>
+                    ) : (
+                        <ul>
+                            {movieList.map((movie) => (
+                                <MovieCard key={movie.id} movie={movie}/>
+                            ))}
+                        </ul>
+                    )}
+                </section>
+            </div>
+        </main>
     )
 }
 
